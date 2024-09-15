e811fb3ae4a56c7bb37f886259bb1e46e768558e
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-09-15 16:43:10 -0300

    client and server

---
+++ ipc/client.c
@@ -0,0 +1,88 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+int THREADS_COUNT = 0;
+
+typedef struct {
+    char *buffer;
+    char operation; 
+    double operand; 
+    int sock;
+} threadArgs;
+
+void* calculate(void* arg);
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+ 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+ 
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+ 
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+ 
+    while(1){
+        char buffer[1024] = {0};
+        char operation;
+        double operand;
+
+        printf("\n\nEnter operation and operand: ");
+        scanf("%c %lf", &operation, &operand);
+        
+	    pthread_t th;
+        threadArgs *args = (threadArgs *)malloc(sizeof(threadArgs));
+        args->buffer = buffer;
+        args->operation = operation;
+        args->operand = operand;
+        args->sock = sock;
+
+	    if (pthread_create(&th, NULL, calculate, args) != 0) {
+        	perror("pthread_create failed");
+        	exit(EXIT_FAILURE);
+    	}
+
+        operation = "";
+        operand = 0;
+     }
+
+    return 0;
+}
+
+void* calculate(void* arg) {
+    threadArgs* args = (threadArgs*) arg;
+    char *buffer = args->buffer;
+    char operation = args->operation;
+    double operand = args->operand;
+    int sock = args->sock;
+
+	THREADS_COUNT++;
+	
+    sprintf(buffer, "%c %lf", operation, operand);
+	send(sock, buffer, strlen(buffer), 0);
+    printf("Request sent %d -> %c %f\n", THREADS_COUNT, operation, operand);
+    memset(buffer, 0, sizeof(buffer));
+    read(sock, buffer, 1024);
+    printf("Result: %s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));
+	return NULL;
+}
---
+++ ipc/server.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <time.h>
+
+#define PORT 8080
+ 
+double calculate(char operation, double operand);
+
+void my_sleep(int seconds) {
+    struct timespec req;
+    req.tv_sec = seconds;          // segundos
+    req.tv_nsec = 0;               // nanosegundos (0 neste caso)
+
+    nanosleep(&req, NULL);         // Chama nanosleep com o tempo especificado
+}
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+ 
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+ 
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+ 
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+ 
+    char buffer[1024] = {0};
+    double operand;
+    char operation;
+ 
+    while(1) {
+        read(new_socket, buffer, 1024);
+        sscanf(buffer, "%c %lf", &operation, &operand);
+        double result = calculate(operation, operand);
+	    sprintf(buffer, "%lf", result);
+        send(new_socket, buffer, strlen(buffer), 0);
+        memset(buffer, 0, sizeof(buffer));
+    }
+ 
+    return 0;
+}
+ 
+double calculate(char operation, double operand) {
+    my_sleep(3);
+    printf("Calculating %c %lf\n", operation, operand);
+    double result = 0;
+    switch(operation) {
+        case '+':
+            result = operand + operand;
+            break;
+        case '*':
+            result = operand * operand;
+            break;
+        case 's':
+            result = sqrt(operand);
+            break;
+        case '^':
+            result = exp(operand);
+            break;
+        case '!':
+            result = 1;
+            for(int i = 1; i <= operand; i++) {
+                result *= i;
+            }
+            break;
+        default:
+            printf("Invalid operation\n");
+    }
+    return result;
+}
+ 
